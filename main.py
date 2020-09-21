7966cb72f20aa170e31df44fdb829d86b849dd5f
Michael Heine <michael.heine1@gmx.net>
2020-09-21 02:15:16 +0200

    Deleted legacy code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""Interface for generating test cases."""
+"""Interface for generating test cases, setting up and running the simulator."""
 
 from fuel_consumption_test_generator import FuelConsumptionTestGenerator
 from xml_converter.xml_to_bng_files import convert_test
@@ -7,7 +7,6 @@ from test_execution.test_execution import run_test_case
 
 if __name__ == '__main__':
     gen = FuelConsumptionTestGenerator()
-
     while True:
         for paths in gen.get_test():
             dbe = paths[0]
